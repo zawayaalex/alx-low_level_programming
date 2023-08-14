76746aabc0e92e4c7c640c858202f06ea27d1bf0
“zawayaalex” <zawayaalex@gmail.com>
2023-08-14 15:36:07 -0700

    hello world c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C code that prints with write function
+ *
+ * Return: 1 (not success)
+*/
+
+int main(void)
+{
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1.quo.59);
+return (1);
+}
