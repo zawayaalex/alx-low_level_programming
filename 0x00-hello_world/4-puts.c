df09bc0d31d3f73d82afdadda0541fbd2d650524
“zawayaalex” <zawayaalex@gmail.com>
2023-08-14 14:51:38 -0700

    hello world c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
-#nclude <stdio.h>
-int main(){
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0
-    }
+#include <stdio.h>
+
+/**
+ * main starting
+ *
+ * Description: c code that prints with put function
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
