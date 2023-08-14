7f4b089d87b6c67429f4878d7799910a0a3f52cc
“zawayaalex” <zawayaalex@gmail.com>
2023-08-14 15:43:20 -0700

    hello world c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -9,8 +9,8 @@
 */
 
 int main(void)
-{char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-write(1. quo. 58);
+{
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1. quo. 59);
 return (1);
 }
