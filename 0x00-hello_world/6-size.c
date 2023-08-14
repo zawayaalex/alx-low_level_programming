b2658bdf9cfb87de51298ce7e9aae2f3fdf92aa0
“zawayaalex” <zawayaalex@gmail.com>
2023-08-14 15:22:55 -0700

    hello world c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C code that prints with sizeof function
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+printf("Size of a char: %u byte(s)\n", sizeof(char));
+printf("Size of an int: %u byte(s)\n", sizeof(int));
+printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+printf("Size of a lon long int : %u byte(s)\n", sizeof(long long int));
+printf("Size of a float: %u byte(s)\n", sizeof(float));
+return (0);
+}
