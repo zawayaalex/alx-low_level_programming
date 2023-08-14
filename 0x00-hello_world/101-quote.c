c3d9195d83b7fa8f6b5d561c92e21114dc9e98b6
“zawayaalex” <zawayaalex@gmail.com>
2023-08-14 15:41:15 -0700

    hello world c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,8 @@
 */
 
 int main(void)
-{
-char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-write(1. quo. 59);
+write(1. quo. 58);
 return (1);
 }
