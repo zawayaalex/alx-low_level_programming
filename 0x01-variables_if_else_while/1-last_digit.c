a76bae5530bc969b6796499cc075f986da55dd11
“zawayaalex” <zawayaalex@gmail.com>
2023-08-14 20:01:02 -0700

    yes

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C code that prints with put function
+ *
+ * Return: 0 (success)
+*/
+
+
+int main(void)
+{
+  int n, digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	digit = n % 10;
+	if (digit > 5)
+	  printf("")
+	    elde if (digit == 0)
+	    printf("")
+	    else if (digit < 6 && digit !=0)
+	      printf("")
+	return (0);
+}
