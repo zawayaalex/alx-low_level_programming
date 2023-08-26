53944b8d6666a2af695155fa6551985bf1aa633b
“zawayaalex” <zawayaalex@gmail.com>
2023-08-25 17:48:59 -0700

    yes

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,26 @@
 #include "main.h"
 
 /**
-* concat
+* _strcat - function that concatenates two strings.
+*
+*
+* @dest: input pointer
+* @src: source input
+*
+* Return: pointer to result @dest
 */
 
-char *_stract(char *dest, char *src)
-int c, c2
+char *_strcat(char *dest, char *src)
+
+int c, c2;
+
 c = 0;
+/*find array size*/
 while (dest[c])
 c++;
+/*itirate*/
 for (c2 = 0; src[c2] ; c2++)
+/*append*/
 dest[c++] = src[c2];
 return (dest);
 }
