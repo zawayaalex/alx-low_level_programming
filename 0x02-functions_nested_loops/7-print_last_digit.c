acf9c2177e36305f5f2c5848eec4e56fbc7c4a1a
“zawayaalex” <zawayaalex@gmail.com>
2023-09-19 15:09:47 -0700

    yes

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,9 +12,9 @@ int print_last_digit(int n)
 if (n % 10 >= 0)
 {
 return (n % 10 );
-_putchar(n % 10 + n % 10);
+_putchar(n % 10 + '0');
 }
 else
 return (-n % 10 + n % 10);
-_putchar(-n % 10 + -n % 10);
+ _putchar(-n % 10 + '0');
 }
