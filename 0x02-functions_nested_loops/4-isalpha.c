372e4a460ad3a9f28cc70e9417721200551bff43
“zawayaalex” <zawayaalex@gmail.com>
2023-09-19 14:23:28 -0700

    yes

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alpha
+ * @c: letter to check
+ * Return: 1 if alpha , 0 if not
+ */
+
+int _isalpha(int c)
+{
+return ((c => 'a' && c =< 'z') || (c => 'A' && c =< 'Z'))
+}
