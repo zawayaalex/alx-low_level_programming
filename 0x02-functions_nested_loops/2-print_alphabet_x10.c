ee66912277ed54a1adecf21cf19c06097ddbf0f4
“zawayaalex” <zawayaalex@gmail.com>
2023-09-19 13:57:05 -0700

    yes

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet 10 times
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+char c;
+for (int i = 0; i < 10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
