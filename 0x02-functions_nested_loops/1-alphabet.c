37c4dde4a050b6131604829b5b27de46d388de78
“zawayaalex” <zawayaalex@gmail.com>
2023-09-19 13:49:24 -0700

    yes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet
+ * main - no main
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+for (char c = a; c <= z; c++)
+{
+_putchar(c);
+}
+_putchar('\n')
+}
