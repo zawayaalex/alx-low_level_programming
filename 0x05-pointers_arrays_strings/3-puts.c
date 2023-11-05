64f16d70d74fa24aadcfb7c0ab15d02cba117f69
root <zawayaalex@gmail.com>
2023-11-05 14:03:49 -0800

    pointers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,16 +6,16 @@ int n = 0;
 
 if (*str == '\0')
 {
-n = 0;
+_putchar(*str);
+_putchar('\n');
 }
 else
 {
 do {
+_putchar(*(str + n));
 n++;
 } while (*(str + n) != '\0');
 }
 
-for (int i = 0; i < n; i++)
-putchar(*str + n);
-putchar('\n');
+_putchar('\n');
 }
