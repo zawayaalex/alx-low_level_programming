19003ecfbbc96e6c6637d6add0d4e29b8aa9ad84
root <zawayaalex@gmail.com>
2023-11-05 13:38:00 -0800

    pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,16 +4,15 @@
 * _strlen - function that returns the lenth of a string
 * @s: for the string
 * @n: for the length
-* 
+* Return: 0 if empty, n if not 
 */
 
 int _strlen(char *s)
 {
-int n = 0;
 
+int n = 0;
 if (*s == '\0')
 return 0;
-
 do
 {
 n++;
