5f16df8b3313f9eced1eca15dda5f09a31cdd558
root <zawayaalex@gmail.com>
2023-11-06 05:42:06 -0800

    pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+File Edit Options Buffers Tools C Help                                                                                                             
+#include "main.h"
+/**
+* puts2 - prints string
+* @str: string
+*/
+
+void puts2(char *str)
+{
+int n = 0;
+
+if (*str == '\0')
+{
+_putchar('\n');
+
+}
+else
+{
+do {
+_putchar(*(str + n));
+n = n + 2;
+} while (*(str + n) != '\0');
+
+_putchar('\n');
+}
+}
