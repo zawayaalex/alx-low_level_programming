6dfca887f3d18be62f6ae774ecdc3800aaad9798
root <zawayaalex@gmail.com>
2023-11-06 11:24:28 -0800

    yes

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,12 +9,14 @@
 
 void print_array(int *a, int n)
 {
+int i = 0;
 n--;
-for (int i = 0; i < n; i++)
-{
-printf(a[i]);
+
+do {
+printf(*a[i]);
 printf(" ,");
-}
+i++;
+} while (i < n)
 printf(a[n]);
 printf("\n");
 }
