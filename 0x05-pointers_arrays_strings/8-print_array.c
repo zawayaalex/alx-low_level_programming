76ca2d6b02cab89a35a124ace4904f49b296e5d7
root <zawayaalex@gmail.com>
2023-11-06 11:18:15 -0800

    yes

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/** print_array - prints array of n
+* @a: the array
+* n: the number
+*
+*/
+
+void print_array(int *a, int n)
+{
+while (*(s + n) != '\0')
+{
+n++;
+}
+n--;
+for (int i = 0, i < n, i++)
+{
+printf(n[i]);
+printf(" ,");
+}
+printf(a[n]);
+printf("\n");
+}
