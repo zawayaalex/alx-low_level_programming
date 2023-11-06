b7d3af244df302d431825c9c5dc3a9bd0ea232f8
root <zawayaalex@gmail.com>
2023-11-06 05:43:07 -0800

    pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,3 @@
-File Edit Options Buffers Tools C Help                                                                                                             
 #include "main.h"
 /**
 * puts2 - prints string
