06cf6d7d09467a131787adeb3a1217fc8c1b2ad6
root <zawayaalex@gmail.com>
2023-11-08 08:40:54 -0800

    2nd pointers 2nd

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -37,7 +37,7 @@ void rev_string(char *n)
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, i = 0, j = 0, digits = 0;
+	int extra = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
 
 	while (*(n1 + i) != '\0')
@@ -48,7 +48,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	j--;
 	if (j >= size_r || i >= size_r)
 		return (0);
-	while (j >= 0 || i >= 0 || overflow == 1)
+	while (j >= 0 || i >= 0 || extra == 1)
 	{
 		if (i < 0)
 			val1 = 0;
@@ -58,11 +58,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			val2 = 0;
 		else
 			val2 = *(n2 + j) - '0';
-		temp_tot = val1 + val2 + overflow;
+		temp_tot = val1 + val2 + extra;
 		if (temp_tot >= 10)
-			overflow = 1;
+			extra = 1;
 		else
-			overflow = 0;
+			extra = 0;
 		if (digits >= (size_r - 1))
 			return (0);
 		*(r + digits) = (temp_tot % 10) + '0';
