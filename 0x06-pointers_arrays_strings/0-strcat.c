1343943bd09b92fd8ad31cc0b5729eb7921ea242
root <zawayaalex@gmail.com>
2023-11-08 07:11:38 -0800

    2nd pointers 2nd

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -23,27 +23,4 @@ i++;
 }
 *(dest + i) = '\0';
 return (dest);
-}#include "main.h"
-
-/**
- * _strcat - appends 2 strings
- * @dest: the 1st one
- * @src: the 2nd one
- */
-
-char *_strcat(char *dest, char *src)
-{
-  int i = 0 , j = 0, n = 0;
-  while (*(dest+i) != '\0')
-    {
-      i++;
-    }
-  while (*(src+j) != '\0')
-    {
-      j++;
-    }
-  while (j < i + j)
-    {
-      dest[j] = src[n];
-      n++;
-      j++
+}
