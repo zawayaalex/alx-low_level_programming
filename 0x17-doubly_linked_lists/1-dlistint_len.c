e2768955183a610b21dac09941451aefb5be9f5b
“zawayaalex” <zawayaalex@gmail.com>
2024-03-21 19:43:54 -0700

    lists

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-
+11;rgb:0000/0000/0000
 /**
  * dlistint_len - Returns the number of elements in a dlistint_t list.
  * @h: Pointer to the head of the list.
@@ -8,13 +8,11 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t count = 0;
-
-    while (h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
-
-    return count;
+size_t count = 0;
+while (h != NULL
+{
+count++;
+h = h->next;
+}
+return count;
 }
